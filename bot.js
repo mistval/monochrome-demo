261037970567186ba9a08407c6f21bb3bec3ddcd
mistval <randallschmidt1@gmail.com>
2017-11-22 15:58:52 -0600

    change config.json to config.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 let monochrome = require('monochrome-bot');
 
-let configFilePath = __dirname + '/config.json';
+let configFilePath = __dirname + '/config.js';
 let commandsDirectoryPath = __dirname + '/commands';
 let messageProcessorsDirectoryPath = __dirname + '/message_processors';
 let settingsFilePath = __dirname + '/server_settings.json';
